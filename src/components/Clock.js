2e7ccb2e42208178bc2a0926902546b13dbf1d91
Derrick <derrickryan10@gmail.com>
2022-06-29 18:43:44 -0400

    Add clock functions

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -6,6 +6,23 @@ export class Clock extends Component {
     this.state = {date: new Date()};
   }
 
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
   render() {
     return (
       <h2>{this.state.date.toLocaleTimeString()}</h2>

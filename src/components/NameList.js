5bdfed68a1563f2ae306c701fa3c8e3cb40bb7dd
Derrick <derrickryan10@gmail.com>
2022-07-01 10:35:25 -0400

    Add keys to list items

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function NameList() {
     const names = ['Derrick', 'Peter', 'Bruce'];
-    const nameList = names.map(name => <h2>{name}</h2>);
+    const nameList = names.map((name, index) => <h2 key={index}>{name}</h2>);
   return <div>{nameList}</div>
 }
 
